2e5617c5b93974c031c51cd66c9ebcd525a0aa7c
virajsuryawanshi <virajsury@gmail.com>
2022-09-23 18:19:24 +0530

    webtask

---
+++ my-app/src/Components/Content.js
@@ -0,0 +1,139 @@
+import React from 'react';
+
+  function Content() {
+    return (
+       <div>
+         <section class="">
+              <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+                <div class="carousel-inner">
+                  <div class="carousel-item active">
+                     <img class="d-block w-100" src="Images/trek.jpg" alt="First slide" style={{height:"auto"}}/>
+                  </div>
+                  <div class="carousel-item">
+                     <img class="d-block w-100 " src="Images/banner-2.jpg" alt="Second slide" style={{height:"auto"}}/>
+                  </div>
+                  <div class="carousel-item">
+                     <img class="d-block w-100" src="Images/t-2.jpg" alt="Third slide" style={{height:"auto"}}/>
+                  </div>
+                </div>
+               </div>
+        </section>   
+
+        <section class="bg-light" id="vision">
+          <div class="container">
+            <div class="row">
+              <div class="col-sm-12 col-md-12">
+                 <h3 class="text-center mt-4 text-secondary">Vision</h3>
+               </div>
+              </div>
+             <div class="row">
+                <p class="mt-4 mb-5">Keeping the core values and the ethics in center, Traveler's Paradise – the NGO will be a benchmark in training the youth for a better situation. The NGO will work for enhancement of all good qualities in the modern youth with a brighter way…</p>
+             </div>   
+          </div>   
+        </section>   
+
+        
+
+       <section class=""  id="objective">
+        <div class="container">
+          <div class="col-sm-12 col-md-12 mb-4">
+             <h3 class="text-center text-secondary mt-4">Objectives</h3>
+         </div>
+       <div class="accordion" id="accordionExample">
+        <div class="card">
+         <div class="card-header" id="headingOne">
+            <h2 class="mb-0">
+            
+            </h2>
+         </div>
+
+         <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
+            <div class="card-body">
+            The idea of establishing NGO came up during the various activities with other NGOs as a part of collaboration with NSS/NCC in college activities. It was a very clear view behind the organization that it will be for young people and specially students development activities will be given priority.
+
+Keeping the ethics and value system in center, the NGO has thought for such activities, which will fill valuable qualities in the youth. NGO is thankful to all the young students of various colleges who are voluntarily giving a boost to the activities. For spreading awareness in the field of adventure, NGO has planned some very charming events as per regular basis.
+            </div>
+         </div>
+      </div>
+      
+      
+      </div>
+    </div> 
+    </section>
+
+    
+    <section class="bg-light mt-5" id="team">    
+     <div class="container">
+      <div class="row text-center">
+        <div class="col-sm-12 col-md-12 mb-4">
+            <h2 class="text-center mt-4 text-secondary">Our Team</h2>
+         </div>
+        <div class="col-md-2">
+          <div class="testimonial mb-3">
+           <div class="avatar mx-auto">
+            <img src="https://media-exp1.licdn.com/dms/image/C5603AQFFT2ITXxmPGA/profile-displayphoto-shrink_800_800/0/1658514196066?e=1669248000&v=beta&t=8TD3ZotQYvQ_v3DBJBrg4PXPofzKmiYhHvq6C15Hcug" class="rounded-circle z-depth-1 img-fluid"/>
+          </div>
+          <h4 class="font-weight-bold dark-grey-text mt-4">Akash Barhate</h4>
+          <h6 class="font-weight-bold blue-text my-3">Team Member1</h6>
+          <p class="font-weight-normal dark-grey-text">
+          Pursuing PG-DAC from CDAC Mumbai
+          Bhusawal, Maharashtra, India</p>
+        </div>
+      </div>
+
+      <div class="col-md-2">
+        <div class="testimonial mb-5">
+          <div class="avatar mx-auto">
+            <img src="https://media-exp1.licdn.com/dms/image/C4D03AQGv_fw0ss_8sg/profile-displayphoto-shrink_800_800/0/1646397049395?e=1669248000&v=beta&t=eCTbkZwDckVR-yMx62vTZ7JardjNskFJM7B3ve1_XYI" class="rounded-circle z-depth-1 img-fluid"/>
+          </div>
+          <h4 class="font-weight-bold dark-grey-text mt-4">Pritesh Kolhe</h4>
+          <h6 class="font-weight-bold blue-text my-3">Team Member2</h6>
+          <p class="font-weight-normal dark-grey-text">Pursuing PG-DAC from CDAC Mumbai
+Bharuch, Gujarat, India .</p>
+        </div>
+
+      </div>
+      <div class="col-md-2">
+        <div class="testimonial mb-5">
+          <div class="avatar mx-auto">
+            <img src="https://media-exp1.licdn.com/dms/image/C4D03AQH3CwKzHj__6A/profile-displayphoto-shrink_800_800/0/1654674771480?e=1669248000&v=beta&t=rv1fvfnZQVmYv_K74tNln3fZ1hoKrI8tBGoEyzUlldw" class="rounded-circle z-depth-1 img-fluid"/>
+          </div>
+          <h4 class="font-weight-bold dark-grey-text mt-4">Viraj Suryawanshi</h4>
+          <h6 class="font-weight-bold blue-text my-3">Team Member3</h6>
+          <p class="font-weight-normal dark-grey-text">
+          Pursuing PG-DAC from CDAC Mumbai
+Nashik, Maharashtra, India </p>
+        </div>
+        </div>
+        <div class="col-md-2">
+        <div class="testimonial mb-5">
+          <div class="avatar mx-auto">
+            <img src="https://media-exp1.licdn.com/dms/image/C4D03AQEueUrsZpza5Q/profile-displayphoto-shrink_800_800/0/1658299220762?e=1669248000&v=beta&t=0mmtseoS3ry1jAoldQwhrr5x1EGArldx-1D8pZA_Gy4" class="rounded-circle z-depth-1 img-fluid"/>
+          </div>
+          <h4 class="font-weight-bold dark-grey-text mt-4">Rajesh Patil</h4>
+          <h6 class="font-weight-bold blue-text my-3">Team Member4</h6>
+          <p class="font-weight-normal dark-grey-text">Pursuing PG-DAC from CDAC Mumbai
+          Pune, Maharashtra, India</p>
+        </div>
+
+      </div><div class="col-md-2">
+        <div class="testimonial mb-5">
+          <div class="avatar mx-auto">
+            <img src="https://media-exp1.licdn.com/dms/image/C4D03AQH3Jbq1Kq7H2w/profile-displayphoto-shrink_200_200/0/1654010367326?e=1669248000&v=beta&t=1rh2h_bHWMtvllGvh5o3Bqkp5KgHolSd82Q25fVZSAY" class="rounded-circle z-depth-1 img-fluid"/>
+          </div>
+          <h4 class="font-weight-bold dark-grey-text mt-4">Vasim Aaga</h4>
+          <h6 class="font-weight-bold blue-text my-3">Team Member5</h6>
+          <p class="font-weight-normal dark-grey-text">Pursuing PG-DAC from CDAC Mumbai
+          Urun-Islāmpur, Maharashtra, India</p>
+        </div>
+
+      </div>
+      
+      </div>
+    </div> 
+   </section> 
+   </div>
+    );
+  }
+  
+  export default Content;
